e2ab66e0bdfa7dee0fe9ec9acb3b34198f767a10
Caio Cesar Mekacheski de Araújo <106687263+CaioMekacheski@users.noreply.github.com>
2023-11-10 00:11:17 -0300

    Implementação do botão cadastrar

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -5,7 +5,7 @@
 
 /**
  *
- * @author Adm
+ * @author caio
  */
 public class cadastroVIEW extends javax.swing.JFrame {
 
@@ -140,6 +140,7 @@ public class cadastroVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_cadastroNomeActionPerformed
 
     private void btnCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarActionPerformed
+        
         ProdutosDTO produto = new ProdutosDTO();
         String nome = cadastroNome.getText();
         String valor = cadastroValor.getText();
@@ -147,6 +148,7 @@ public class cadastroVIEW extends javax.swing.JFrame {
         produto.setNome(nome);
         produto.setValor(Integer.parseInt(valor));
         produto.setStatus(status);
+        
         
         ProdutosDAO produtodao = new ProdutosDAO();
         produtodao.cadastrarProduto(produto);
