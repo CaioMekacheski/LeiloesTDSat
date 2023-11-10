80283cc61c6af8257f1bd7871772dd69f1c9d462
Caio Cesar Mekacheski de Araújo <106687263+CaioMekacheski@users.noreply.github.com>
2023-11-09 23:53:03 -0300

    Atualização em conectaDAO

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -2,6 +2,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 
@@ -13,20 +15,32 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author Adm
+ * @author caio
  */
 public class conectaDAO {
     
-    public Connection connectDB(){
+    public Connection connectDB()
+    {
+        
         Connection conn = null;
+        System.out.println(conn);
         
-        try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=password");
+        try 
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11", "root", "password");
             
-        } catch (SQLException erro){
+        } 
+        catch (SQLException erro)
+        {
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        } 
+        catch (ClassNotFoundException ex) 
+        {
+            Logger.getLogger(conectaDAO.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Erro jdbc driver");
         }
+        System.out.println(conn);
         return conn;
     }
     
