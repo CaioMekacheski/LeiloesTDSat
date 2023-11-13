b5f63cbfbfe42e2921299a05af2fe2b1d68c5cdb
Caio Cesar Mekacheski de Araújo <106687263+CaioMekacheski@users.noreply.github.com>
2023-11-13 19:50:02 -0300

    Criação da funcao listarVendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -45,6 +45,33 @@ public class ProdutosDAO
         }   
     }
     
+    public ArrayList<ProdutosDTO> listarVendidos()
+    {
+        conn = new conectaDAO().connectDB();
+        try 
+        { 
+            prep = conn.prepareStatement("select * from Produtos where status = Vendido");
+            resultset = prep.executeQuery();
+            
+            while(resultset.next())
+            {  
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getDouble("valor"));
+                produto.setStatus(resultset.getString("status"));
+                listagem.add(produto);
+            }
+            System.out.println(listagem);
+        } 
+        catch (SQLException ex) 
+        {
+            Logger.getLogger(ProdutosDAO.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Erro ao acessar dados");
+        }
+        return listagem;
+    }
+    
     public ArrayList<ProdutosDTO> listarProdutos()
     {
         conn = new conectaDAO().connectDB();
